851f8b769b4d8809ac2466abf4771cae89005e1f
Alexander <alexgomezgomez15@gmail.com>
2023-09-15 10:12:09 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import AuthPage from "./pages/AuthPage";
+import NewOrderPage from "./pages/NewOrderPage";
+import OrderHistoryPage from "./pages/OrderHistoryPage";
+import {Routes, Route} from 'react-router-dom';
+import Nav from './components/Nav';
+import { getUser } from "./utilities/users-service";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [user, setUser] = useState(getUser());
+
+  return( 
+  <main className="App">
+    {user ? 
+    (
+    <>
+    <Nav user={user} setUser={setUser} />
+    <Routes>
+      <Route path='/orders/new'  element={<NewOrderPage />}/>
+      <Route path='/orders' element={<OrderHistoryPage />}/>
+    </Routes> 
+    </>)
+    : 
+    <AuthPage setUser={setUser} />}
+    
+    </main>
   );
 }
 
